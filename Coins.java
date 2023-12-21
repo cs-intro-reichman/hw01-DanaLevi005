854ba672e2a75ccd128f3772b48054dab7434255
danalevi005 <danalevi.pearl@post.runi.ac.il>
2023-12-21 16:39:05 +0200

    homework

--- Coins.java
+++ Coins.java
@@ -3,7 +3,11 @@
  * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
  */
 public class Coins {
-	public static void main(String[] args) {
-		// Put your code here
-	}
+    public static void main(String[] args) { 
+        int cents = Integer.parseInt(args[0]);
+        int quarter = cents / 25;
+        int cent = cents % 25;
+        System.out.println("Use " + quarter + " quarters and " + cent + " cents ");
+        
+    }
 }
--- GenThree.java
+++ GenThree.java
@@ -3,7 +3,21 @@
  * prints them, and then prints the minimal number that was generated.
  */
 public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int first_random = (int)(Math.random() * (b - a) + a);
+        int second_random = (int)(Math.random() * (b - a) + a);
+        int third_random = (int)(Math.random() * (b - a) + a);
+        int minimal = Math.min(first_random, second_random);
+        minimal = Math.min(minimal, third_random);
+
+        System.out.println(first_random);
+        System.out.println(second_random);
+        System.out.println(third_random);
+        System.out.println("The minimal generated number was " + minimal);
+        
+        
+    }
 }
